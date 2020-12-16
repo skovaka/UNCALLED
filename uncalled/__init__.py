483bdaa779d293c24988017655b65d1e883e43f9
Sam Kovaka <skovaka@gmail.com>
2020-12-16 09:58:53 -0500

    Fixed import issue with realtime mode

--- uncalled/__init__.py
+++ uncalled/__init__.py
@@ -1,5 +1,2 @@
 from _uncalled import *
-#from uncalled.minknow_client import Client
-from uncalled import index, pafstats, sim_utils, args, debug
-
-#TODO further flatten module structure, then fix uncalled script
+from uncalled import index, pafstats, sim_utils, args, debug, minknow_client
