e5e2ce0c848969b9b28c3b18183b07274c05f0bf
skovaka <skovaka@gmail.com>
2021-10-07 16:12:54 -0400

    Worked on hover info

--- uncalled/vis/plotly/dotplot.py
+++ uncalled/vis/plotly/dotplot.py
@@ -21,7 +21,7 @@ class DotplotParams(config.ParamGroup):
     _name = "dotplot"
 DotplotParams._def_params(
     ("tracks", None, None, "DTW aligment tracks"),
-    ("layers", ["model_diff"], None, ""),
+    ("dtw_layers", ["model_diff"], None, ""),
     ("track_colors", ["#AA0DFE", "#1CA71C", "#6A76FC"], list, ""),
 )
 
@@ -29,9 +29,9 @@ class Dotplot:
     def __init__(self, *args, **kwargs):
         self.conf, self.prms = config._init_group("dotplot", *args, **kwargs)
         self.tracks = self.prms.tracks #TODO do this better
-        self.prms.layers = list(parse_layers(self.prms.layers))
+        #self.prms.layers = list(parse_layers(self.prms.layers))
 
-        column_widths=[5]+[1]*len(self.prms.layers)
+        column_widths=[5]+[1]*len(self.prms.dtw_layers)
 
         self.fig = make_subplots(
             rows=2, cols=len(column_widths), 
@@ -48,7 +48,8 @@ class Dotplot:
         if len(self.tracks) == 2:
             self.tracks[0].compare(self.tracks[1])
 
-        hover_layers = ["middle","kmer","current","dwell","model_diff"]
+        hover_layers = ["middle","kmer","current","dwell"]#,"model_diff"]
+        hover_layers += (l for l in self.prms.dtw_layers if l not in {"current","dwell"})
         hover_data = dict()
 
         for i,track in enumerate(self.tracks):
@@ -69,9 +70,9 @@ class Dotplot:
                     hoverinfo="skip",
                 ), row=2, col=1)
 
-                for j,layer in enumerate(self.prms.layers):
+                for j,layer in enumerate(self.prms.dtw_layers):
                     self.fig.add_trace(go.Scatter(
-                        x=dtw[layer[1]], y=dtw.index-0.5, #TODO try vhv
+                        x=dtw[layer], y=dtw.index-0.5, #TODO try vhv
                         name=track.name, 
                         line={
                             "color" : self.prms.track_colors[i], 
@@ -96,10 +97,10 @@ class Dotplot:
         hover_rows = [
             "<b>" + track.coords.ref_name + ":%{y:,d} [%{text}]</b>"
         ]
-        labels = [
-            "Current (pA): ", 
-            "Dwell (ms): ", 
-            "Model pA Diff: "]
+        labels = [LAYERS["dtw"][l].label for l in hover_layers[2:]]
+        #    "Current (pA): ", 
+        #    "Dwell (ms): ", 
+        #    "Model pA Diff: "]
 
         for i,label in enumerate(labels):
             s = label
@@ -109,7 +110,7 @@ class Dotplot:
                 fields.append(
                     '<span style="color:%s;float:right"><b>%%{customdata[%d]:.2f}</b></span>' % 
                     (self.prms.track_colors[j], k))
-            hover_rows.append(s + ", ".join(fields))
+            hover_rows.append(s + ": " + ", ".join(fields))
 
         self.fig.add_trace(go.Scatter(
             x=hover_coords, y=hover_coords.index,
@@ -131,9 +132,9 @@ class Dotplot:
         self.fig.update_yaxes(row=2, col=1,
             title_text="Reference (%s)" % aln["ref_name"])
 
-        for i,layer in enumerate(self.prms.layers):
+        for i,layer in enumerate(self.prms.dtw_layers):
             self.fig.update_xaxes(row=2, col=i+2,
-                title_text=LAYERS[layer[0]][layer[1]].label)
+                title_text=LAYERS["dtw"][layer].label)
 
         axis_kw = dict(
             showspikes=True,
@@ -164,12 +165,12 @@ OPTS = (
     Opt(("-f", "--out-format"), default="svg", help="Image output format. Only has an effect with -o option.", choices={"pdf", "svg", "png"}),
     Opt(("-R", "--ref-bounds"), "track_io", type=str_to_coord),
     Opt(("-l", "--read-filter"), "track_io", type=parse_read_ids),
-    Opt(("-L", "--layers"), "dotplot", type=comma_split),
+    Opt(("-L", "--layers"), "dotplot", "dtw_layers", type=comma_split),
 )
 
 def main(conf):
     """plot a dotplot"""
-    conf.track_io.layers = ["start", "length", "middle", "current", "kmer", "base"] + conf.dotplot.layers
+    conf.track_io.layers = ["start", "length", "middle", "current", "dwell", "kmer", "base"] + conf.dotplot.dtw_layers
     io = TrackIO(conf=conf)
 
     for read_id, tracks in io.iter_reads():
