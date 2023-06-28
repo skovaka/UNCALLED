Fixed hard clipping bug, added normalization parameters

uncalled/dtw/dtw.py
@@ -120,7 +120,6 @@ def dtw_worker(p):
 
     #tracks.norm_params = pd.read_csv("/scratch1/skovaka/curlcakes/unm/nanopolish/eventalign/npl_unm_cov100_smry.txt", sep="\t").set_index("read_name").sort_index()
     #tracks.norm_params = pd.read_csv("/scratch1/skovaka/curlcakes/m6a/nanopolish/eventalign/npl_m6a_cov100_smry.txt", sep="\t").set_index("read_name").sort_index()
-    #print(tracks.norm_params)
 
     i = 0
     for bam in bams:
@@ -165,6 +164,7 @@ class GuidedDTW:
 
         self.aln = aln
         self.moves = self.aln.moves #moves.aln
+        #print(self.moves.to_pandas())
         if self.moves.empty():
             sys.stderr.write(f"Warning: moves missing for read {aln.read_id}\n")
             return
@@ -267,7 +267,9 @@ class GuidedDTW:
             self.aln.set_dtw(dtw)
             success = True
 
+        
         if success:
+            #print("ERE")
             #try:
             #if self.conf.mvcmp and self.aln.mvcmp.empty():
             if self.aln.mvcmp.empty():
@@ -280,7 +282,7 @@ class GuidedDTW:
             if (not self.prms.unmask_splice) and aln.seq.is_spliced():
                 mask &= np.array(aln.seq.splice_mask)
 
-            if np.sum(mask) < 100:
+            if np.sum(mask) < self.conf.tracks.min_aln_length:
                 return
 
             mask[0] = mask[-1] = True
@@ -292,6 +294,8 @@ class GuidedDTW:
             return
             #except:
             #    pass
+        #else:
+        #    print("FAIL")
         sys.stderr.write(f"Failed to write alignment for {read.id}\n")
 
 
@@ -302,13 +306,15 @@ class GuidedDTW:
             aln.calc_mvcmp()
 
         na_mask = np.array(aln.dtw.na_mask)
-        dist_mask = np.array(aln.mvcmp.dist) <= 1
+        dist_mask = np.array(aln.mvcmp.dist) <= self.conf.tracks.min_norm_dist
+
+        #print(np.array(aln.mvcmp.dist))
 
         mask = na_mask & dist_mask
-        if np.sum(mask) < 100:
-            if np.sum(na_mask) < 100:
+        if np.sum(mask) < self.conf.tracks.min_aln_length:
+            if np.sum(na_mask) < self.conf.tracks.min_aln_length:
                 return False
-            sys.stderr.write(f"{aln.read_id}\tnofilter\t{np.sum(mask)}\t{len(mask)}\n")
+            #sys.stderr.write(f"{aln.read_id}\tnofilter\t{np.sum(mask)}\t{len(mask)}\n")
             mask = na_mask
         
         ref_current = aln.seq.current.to_numpy()[mask] #self.ref_kmers[aln["mpos"]]
@@ -339,12 +345,15 @@ class GuidedDTW:
         dtw = self.dtw_fn(self.prms, signal, self.evt_start, self.evt_end, self.model.kmer_array(self.seq.kmer.to_numpy()), self.model.instance, _uncalled.PyArrayCoord(bands))
 
         if np.any(dtw.path["qry"] < self.evt_start) or np.any(dtw.path["ref"] < 0):
+            #print("FAAAIIL", dtw.path)
             return False
 
         dtw.fill_aln(self.aln.instance, self.conf.tracks.count_events)
 
         if self.conf.tracks.mask_skips is not None:
             self.aln.mask_skips(self.conf.tracks.mask_skips == "keep_best")
+        
+        #print("HRERE")
 
         return True
 

uncalled/dtw/io/__init__.py
@@ -1,5 +1,4 @@
 """Edit, merge, and ls alignment databases
-        print(d)
 
 subcommand options:
 ls       List all tracks in a database
@@ -254,8 +253,11 @@ def convert_worker(args):
         
 
 def convert_single(conf):
+    conf.tracks.layers.append("moves")
     tracks, ignore_bam = _init_tracks(conf)
     for read_id, aln in tracks.iter_reads(ignore_bam=ignore_bam):
+        aln.calc_mvcmp()
+        dist = np.array(aln.mvcmp.dist)
         tracks.write_alignment(aln)
 
     tracks.close()

uncalled/dtw/moves.py
@@ -9,8 +9,10 @@ import time
 import pandas as pd
 import scipy.stats
 import copy
+import pysam
 
 from _uncalled import _AlnDF, IntervalIndexI64, IntervalIndexI32, moves_to_aln, read_to_ref_moves
+from ..aln import AlnDF
 from ..pore_model import PoreModel
 from ..config import Config
 from ..argparse import Opt
@@ -61,29 +63,31 @@ def sam_to_ref_moves(conf, ref_index, read, sam):
     is_fwd = not sam.is_reverse
     flip_ref = is_fwd == model.reverse
 
+    cig = sam.cigartuples
+    start_shift = cig[0][1] if cig[0][0] == pysam.CHARD_CLIP else 0
+    end_shift = cig[-1][1] if cig[-1][0] == pysam.CHARD_CLIP else 0
+
     ar = np.array(sam.get_aligned_pairs())
     ar = ar[ar[:,1] != None] #TODO keep track of insertion counts
 
-
     if flip_ref:
         ar = ar[::-1]
-        qrys = ar[:,0]
-        #silly trick to make null reverse into REF_NA
+        qrys = ar[:,0] 
         read_len = sam.infer_read_length()
-        qrys[qrys == None] = read_len - INT32_NA - 1
-        qrys = read_len - qrys - 1
+        na = qrys == None
+        qrys[na] = 0
+        qrys = read_len - qrys - 1# + end_shift
+        qrys[na] = INT32_NA
     else:
         qrys = ar[:,0]
-        qrys[qrys == None] = INT32_NA
+        na = qrys == None
+        qrys[na] = 0
+        qrys[na] = INT32_NA
 
     refs = np.array(ref_index.pos_to_mpos(ar[:,1], is_fwd))
     qrys = np.array(qrys, dtype=np.int64)
 
-
     ref_moves = read_to_ref_moves(read_moves, refs, qrys, conf.dtw.del_max, True)
-
-    #if workflow is None:
-    #    workflow = (conf.read_buffer.flowcell, conf.read_buffer.kit)
 
     mkl = MOVE_KMER_LENS[PoreModel.PRESET_MAP.loc[conf.pore_model.get_workflow(), "ont_model"]]
 

uncalled/dtw/train.py
@@ -18,12 +18,19 @@ def init_model(tracks, k):
     currents = list()
     length = 0
 
-    for read in tracks.read_index:
-        print(read.id, read.moves)
-        st = read.template_start
-        en = st + np.sum(read.moves)*5
+    for sam in tracks.bam_in.iter_sam():
+
+        mv = np.array(sam.get_tag("mv"))
+        mv_stride = mv[0]
+        st = sam.get_tag("ts")
+        moves = mv[1:]
+
+        en = st + (np.sum(moves) * mv_stride)
+
+        read = tracks.read_index[sam.query_name]
         c = evdt.get_means(read.signal.to_numpy()[st:en])
-        #currents.append(read.signal[st:en])
+        print(read.id, moves, read.signal)
+
         currents.append(c)
         length += len(c)
         if length >= tracks.conf.train.init_events:
@@ -49,16 +56,18 @@ def init_model(tracks, k):
     tracks.conf.pore_model.pa_stdv = np.std(currents)
     #tracks.set_model(PoreModel((tracks.conf.pore_model, tracks.conf.normalizer.tgt_mean, tracks.conf.normalizer.tgt_stdv)))
     model = PoreModel(params=tracks.conf.pore_model)
+    print("DONE")
     print(model.to_df())
     tracks.set_model(model)
 
 def train(conf):
-    conf.tracks.load_fast5s = True
+    #conf.tracks.load_signal = True
+    conf.tracks.layers.append("moves")
     conf.mvcmp = True
 
     prms = conf.train
 
-    if prms.init_model is not None:
+    if len(prms.init_model) > 0:
         conf.pore_model.name = prms.init_model
     elif not prms.append and prms.kmer_len is not None:
         #conf.pore_model.name = ""